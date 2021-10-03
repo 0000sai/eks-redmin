c9fde80cc3cb5fa63bb72bc298d42ad8c9daf25e
Muhammad Asim <asim@arbisoft.com>
2021-10-03 11:05:14 +0500

    Eks Redmin deployment on Managed Nodes

---
+++ README.md
@@ -0,0 +1,23 @@
+# Redmine AWS EKS Setup
+
+# Prerequisites
+
+1. ACM Setup with your domain
+
+2. packages Installed in provided packages.sh
+
+3. Docker (Optional)
+
+4. VsCode (Optional)
+
+# Steps
+
+1. cd eks/environment/dev
+
+2. aws configure 
+
+3. terraform apply
+
+4. Resources Provisioned VPC, RDS, KMS, AWS Secrets,EKS CLUSTER, EKS Manged Nodes
+
+5. Terraform Destroy
---
+++ eks/environment/dev/eksctl.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+EKS_CLUSTER_NAME="cloudgeeks-ca-eks"
+EKS_VERSION="1.19"
+REGION="us-east-1"
+NODE_TYPE="t3a.medium"
+TOTAL_NODES="2"
+MIN_NODES="1"
+MAX_NOES="2"
+NODE_VOLUME_SIZE_IN_GB="30"
+SSH_KEY_NAME="cloudgeeks-ca-eks"
+
+
+PRIVATE_SUBNET_1=`terraform output private-subnets-ids | sed -n 2p | cut -d'"' -f2`
+PRIVATE_SUBNET_2=`terraform output private-subnets-ids | sed -n 3p | cut -d'"' -f2`
+PRIVATE_SUBNET_3=`terraform output private-subnets-ids | sed -n 4p | cut -d'"' -f2`
+
+
+PUBLIC_SUBNET_1=`terraform output public-subnet-ids | sed -n 2p | cut -d'"' -f2`
+PUBLIC_SUBNET_2=`terraform output public-subnet-ids | sed -n 3p | cut -d'"' -f2`
+PUBLIC_SUBNET_3=`terraform output public-subnet-ids | sed -n 4p | cut -d'"' -f2`
+
+
+
+
+
+
+# Creating a key pair for EC2 Workers Nodes
+
+if [ -d "~/.ssh" ]
+then
+    echo "Directory .ssh exists."
+else
+    echo "Creating a Directory .ssh Directory"
+
+    mkdir -p ~/.ssh
+fi
+
+aws ec2 create-key-pair --key-name $SSH_KEY_NAME --query 'KeyMaterial' --output text > ~/.ssh/$SSH_KEY_NAME.pem
+
+# Eks Cluster Creation
+
+eksctl create cluster \
+  --name $EKS_CLUSTER_NAME \
+  --version $EKS_VERSION \
+  --vpc-private-subnets=$PRIVATE_SUBNET_1,$PRIVATE_SUBNET_2,$PRIVATE_SUBNET_3 \
+  --vpc-public-subnets=$PUBLIC_SUBNET_1,$PUBLIC_SUBNET_2,$PUBLIC_SUBNET_3 \
+  --region "$REGION" \
+  --nodegroup-name worker \
+  --node-type $NODE_TYPE \
+  --nodes $TOTAL_NODES \
+  --nodes-min $MIN_NODES \
+  --nodes-max $MAX_NOES \
+  --ssh-access \
+  --node-volume-size $NODE_VOLUME_SIZE_IN_GB \
+  --ssh-public-key $SSH_KEY_NAME \
+  --appmesh-access \
+  --full-ecr-access \
+  --alb-ingress-access \
+  --node-private-networking \
+  --managed \
+  --asg-access \
+  --verbose 3
+  
+aws eks update-kubeconfig --name $EKS_CLUSTER_NAME --region $REGION
+
+
+# UPDATE YOUR ./kube
+################################################################
+### MUST ###
+###---> aws eks update-kubeconfig --name cloudgeeks-ca-eks --region us-east-1 <---
+##################################################################
+
+# Update Public to Private End Points
+# aws eks update-cluster-config --name cloudgeeks-ca-eks --region us-east-1 \
+# --resources-vpc-config endpointPublicAccess=false,endpointPrivateAccess=true
+
+#END
