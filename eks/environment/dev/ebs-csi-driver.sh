21fa73e1caf9e1b08af77bf1813597830946b796
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-10-23 15:43:16 +0500

    Update ebs-csi-driver.sh

--- eks/environment/dev/ebs-csi-driver.sh
+++ eks/environment/dev/ebs-csi-driver.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #Purpose: Setup EBS CSI Driver
 
-AWS_ACCOUNT="738333021806"   # Update This
+AWS_ACCOUNT="049043308513"          # Update This
 
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-persistent-storage/
 
 # https://docs.aws.amazon.com/eks/latest/userguide/ebs-csi.html
 
-# https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
 
-# https://www.eksworkshop.com/beginner/170_statefulset/ebs_csi_driver/
 
 # 602401143452.dkr.ecr.us-east-1.amazonaws.com/
 
@@ -19,9 +18,40 @@ AWS_ACCOUNT="738333021806"   # Update This
 
 # https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json
 
-curl -o Amazon_EBS_CSI_Driver_Policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json
 
-aws iam create-policy --policy-name Amazon_EBS_CSI_Driver_Policy --policy-document file://Amazon_EBS_CSI_Driver_Policy.json
+#cat << EOF > Amazon_EBS_CSI_Driver.json
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Effect": "Allow",
+#      "Action": [
+#        "ec2:AttachVolume",
+#        "ec2:CreateSnapshot",
+#        "ec2:CreateTags",
+#        "ec2:CreateVolume",
+#        "ec2:DeleteSnapshot",
+#        "ec2:DeleteTags",
+#        "ec2:DeleteVolume",
+#        "ec2:DescribeAvailabilityZones",
+#        "ec2:DescribeInstances",
+#        "ec2:DescribeSnapshots",
+#        "ec2:DescribeTags",
+#        "ec2:DescribeVolumes",
+#        "ec2:DescribeVolumesModifications",
+#        "ec2:DetachVolume",
+#        "ec2:ModifyVolume"
+#      ],
+#      "Resource": "*"
+#    }
+#  ]
+#}
+#
+#EOF
+
+curl -o Amazon_EBS_CSI_Driver.json https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/v0.9.0/docs/example-iam-policy.json
+
+aws iam create-policy --policy-name Amazon_EBS_CSI_Driver --policy-document file://Amazon_EBS_CSI_Driver.json
 
 
 ROLE_ARN=$(kubectl -n kube-system describe configmap aws-auth | grep "rolearn" | awk '{print $2}')
@@ -33,7 +63,7 @@ ROLE_NAME=$(kubectl -n kube-system describe configmap aws-auth | grep "rolearn" 
 
 # https://docs.aws.amazon.com/cli/latest/reference/iam/attach-role-policy.html
 
-aws iam attach-role-policy --policy-arn arn:aws:iam::${AWS_ACCOUNT}:policy/Amazon_EBS_CSI_Driver_Policy --role-name ${ROLE_NAME}
+aws iam attach-role-policy --policy-arn arn:aws:iam::${AWS_ACCOUNT}:policy/Amazon_EBS_CSI_Driver --role-name ${ROLE_NAME}
 
 
 
