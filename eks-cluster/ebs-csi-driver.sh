6d4bd5b15406e6d7f31459a1483878087ad877d4
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-11-10 07:49:31 +0500

    Update ebs-csi-driver.sh

--- eks-cluster/ebs-csi-driver.sh
+++ eks-cluster/ebs-csi-driver.sh
@@ -82,9 +82,12 @@ kubectl get all -A
 
 # Aws-Secret-Manager Secret CSI Driver
 
+cd secret-manager
+
 kubectl apply -f secrets-store-csi-driver
 
 kubectl apply -f aws-provider-installer
+
 
 
 # End
