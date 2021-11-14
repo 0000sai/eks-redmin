0e90811bb88d9366f58a0e78954e3451bc025b1f
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-11-14 10:58:20 +0500

    Update packages.sh

--- redmine/packages/packages.sh
+++ redmine/packages/packages.sh
@@ -29,6 +29,14 @@ rm -f awscliv2.zip
 
 aws --version
 
+# AWS CLIV2
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+aws --version
+
 
 
 # EKSCTL
