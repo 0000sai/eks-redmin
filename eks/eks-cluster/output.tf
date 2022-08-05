0b36b8233e78e172ae0305775380c2062c8a3dc8
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-08-05 11:29:38 +0500

    Update output.tf

--- eks/eks-cluster/output.tf
+++ eks/eks-cluster/output.tf
@@ -1,3 +1,16 @@
+output "vpc-id" {
+  value = module.vpc.vpc-id
+}
+
+output "public-subnet-ids" {
+  value = module.vpc.public-subnet-ids
+}
+
+output "private-subnets-ids" {
+  value = module.vpc.private-subnet-ids
+}
+
+
 output "eks" {
   value = null_resource.eks
 }
