63d2df75db5640a57189bcffdb24688628f96b75
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-08-06 08:56:15 +0500

    Update eks-secrets-creation.sh

--- eks/eks-secrets/eks-secrets-creation.sh
+++ eks/eks-secrets/eks-secrets-creation.sh
@@ -8,29 +8,29 @@ fi
 
 echo "First, deleting the old secret: -credentials"
 
-kubectl delete secrets redmine-db-mysql || true
+kubectl -n redmine delete secrets redmine-db-mysql || true
 
-kubectl delete secrets mysql-database || true
+kubectl -n redmine delete secrets mysql-database || true
 
-kubectl delete secrets rds-admin || true
+kubectl -n redmine delete secrets rds-admin || true
 
-kubectl delete secrets redmine-db-password  || true
+kubectl -n redmine delete secrets redmine-db-password  || true
 
-kubectl delete secrets redmine-secret-key || true
+kubectl -n redmine delete secrets redmine-secret-key || true
 
 
 echo "file found, creating kubernetes secret: -credentials"
 
 source ./redmine.env
 
-kubectl create secret generic redmine-db-mysql --from-literal=REDMINE_DB_MYSQL=${REDMINE_DB_MYSQL} # Rds Endpoint
+kubectl -n redmine create secret generic redmine-db-mysql --from-literal=REDMINE_DB_MYSQL=${REDMINE_DB_MYSQL} # Rds Endpoint
 
-kubectl create secret generic mysql-database --from-literal=MYSQL_DATABASE=${MYSQL_DATABASE} # DB Name
+kubectl -n redmine create secret generic mysql-database --from-literal=MYSQL_DATABASE=${MYSQL_DATABASE} # DB Name
 
-kubectl create secret generic rds-admin --from-literal=REDMINE_DB_USERNAME=${REDMINE_DB_USERNAME} # Rds Admin
+kubectl -n redmine create secret generic rds-admin --from-literal=REDMINE_DB_USERNAME=${REDMINE_DB_USERNAME} # Rds Admin
 
-kubectl create secret generic redmine-db-password --from-literal=REDMINE_DB_PASSWORD=${REDMINE_DB_PASSWORD} # Rds root
+kubectl -n redmine create secret generic redmine-db-password --from-literal=REDMINE_DB_PASSWORD=${REDMINE_DB_PASSWORD} # Rds root
 
-kubectl create secret generic redmine-secret-key --from-literal=REDMINE_SECRET_KEY_BASE=${REDMINE_SECRET_KEY_BASE}
+kubectl -n redmine create secret generic redmine-secret-key --from-literal=REDMINE_SECRET_KEY_BASE=${REDMINE_SECRET_KEY_BASE}
 
 # End
